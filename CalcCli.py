e92734c500ab13f82f25121494cf675626e50272
Md Sami Mujawar <77041014+Sami530@users.noreply.github.com>
2023-02-06 22:38:08 +0530

    Added abs diff option and help section

--- CalcCli.py
+++ CalcCli.py
@@ -4,7 +4,17 @@ import click
 @click.argument('number2')
 @click.argument('method')
 
-def main(number1,number2,method):
+@click.option('--add',help="n1 n2 add")
+@click.option('--sub',help="n1 n2 sub")
+@click.option('--mult',help="n1 n2 mult")
+@click.option('--div',help="n1 n2 div")
+@click.option('--absolute difference',help="n1 n2 abso")
+
+
+def main(number1,number2,method,name):
+
+    
+
     """Add Number 1 and Number 2"""
     if(method == 'add'):
         result = int(number1) + int(number2)
@@ -14,8 +24,10 @@ def main(number1,number2,method):
         result = int(number1) * int(number2)
     elif(method == 'div'):
         result = float(number1) / float(number2)
+    elif(method == 'abso'):
+        result = abs(number1-number2)
     
-    click.echo(click.style((f'Your Computed Answer is {result}'),fg='green',bg='black',bold=True))
+    click.echo(click.style((f'Your Computed Answer is {result}'),fg='cyan',bg='black',bold=True))
 
 if __name__ == '__main__':
     main()
