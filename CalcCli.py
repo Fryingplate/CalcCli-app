51050f4affab791ddf4005268ee2a2c46cb5fe94
Md Sami Mujawar <77041014+Sami530@users.noreply.github.com>
2023-02-07 22:28:22 +0530

    Fixed the mod option

--- CalcCli.py
+++ CalcCli.py
@@ -1,4 +1,5 @@
 import click
+import math
 @click.command()
 @click.argument('number1')
 @click.argument('number2')
@@ -8,14 +9,18 @@ import click
 @click.option('--sub',help="n1 n2 sub")
 @click.option('--mult',help="n1 n2 mult")
 @click.option('--div',help="n1 n2 div")
-@click.option('--absolute difference',help="n1 n2 abso")
+@click.option('--mod',help="n1 n2 mod")
 
 
-def main(number1,number2,method,name):
 
-    
 
-    """Add Number 1 and Number 2"""
+
+
+
+
+def main(number1,number2,method):
+
+    """Help for you :)) """
     if(method == 'add'):
         result = int(number1) + int(number2)
     elif(method == 'sub'):
@@ -26,6 +31,11 @@ def main(number1,number2,method,name):
         result = float(number1) / float(number2)
     elif(method == 'abso'):
         result = abs(number1-number2)
+    elif(method == 'mod'):
+        result = (number1%number2)
+    
+        
+        
     
     click.echo(click.style((f'Your Computed Answer is {result}'),fg='cyan',bg='black',bold=True))
 
